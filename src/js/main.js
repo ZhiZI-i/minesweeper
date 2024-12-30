f1f1ea4868d8fbcb8eb94a894fd3eeee42188899
ZhiZI-i <379116151@qq.com>
2024-12-31 00:21:45 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import { Game } from './models/Game.js';
 
-// 创建一个全局游戏实例
 let game = null;
 
-// 当页面加载完成后初始化游戏
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM加载完成，初始化游戏');
     game = new Game();
